3136595e5d5f737a244fe73cf46181269a7f8d5c
samz <samz@quantum.localdomain>
2024-02-13 10:41:13 -0800

    remove animation and fix skill bars

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,19 +1,24 @@
 jQuery(document).ready(function($) {
 
     /*======= Skillset *=======*/
-    $('.level-bar-inner').css('width', '0');
-    $(window).on('load', function() {
-        $('.level-bar-inner').each(function() {
-            var itemWidth = $(this).data('level');
-            $(this).animate({
-                width: itemWidth
-            }, 800);
-        });
+
+    // Calculate and set the final width directly:
+    $('.level-bar-inner').each(function() {
+        var itemWidth = $(this).data('level');
+
+        // Ensure valid percentage value:
+        if (!/^\d+\%$/.test(itemWidth)) {
+            console.error("Invalid 'data-level' value:", itemWidth);
+            return; // Skip styling if value is invalid
+        }
+
+        $(this).css('width', itemWidth); // Set the width directly
     });
 
     /*======= target="_blank" on external links *=======*/
-    // found here: https://stackoverflow.com/a/4425214
-    $(document.links).filter(function() {
-        return this.hostname != window.location.hostname;
-    }).attr('target', '_blank');
+    // Use event delegation for efficiency:
+    $(document).on('click', 'a[href^="http"]:not([href*="' + window.location.hostname + '"])', function() {
+        $(this).attr('target', '_blank');
+    });
+
 });
